00fdca2fe5b7e0cf925516963b6264bd35b37bfe
MatasVaitkevicius <vaitkevicius.matas@gmail.com>
2024-04-10 00:50:59 +0300

    cloudinit refactored

--- terraform/configure-ec2.sh
+++ terraform/configure-ec2.sh
@@ -25,8 +25,9 @@ sudo curl -L "$COMPOSE_FILE_URL" -o ./docker-compose.yaml
 sudo curl -L "$NGINX_CONF_URL" -o ./docker-compose.yaml
 
 # Get the public IP address of the EC2 instance
-EC2_PUBLIC_IP=$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4)
+TOKEN=$(curl -s -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
 
+EC2_PUBLIC_IP=$(curl -s -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/public-ipv4)
 # Replace the placeholder with the actual IP address
 sed -i "s/{EC2_PUBLIC_IP}/$EC2_PUBLIC_IP/g" ./docker-compose.yaml
 
